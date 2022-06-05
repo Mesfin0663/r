0eceaab737b5716a1a7196bfe724f8d274d61686
Mesfin0663 <87477356+Mesfin0663@users.noreply.github.com>
2022-06-05 09:39:00 +0300

    started

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1 className="logo">Hello </h1>
       <h2 className="description">Start editing to see some magic happen!</h2>
     </div>
   );
